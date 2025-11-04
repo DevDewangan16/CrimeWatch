b31dd7af325e17bb121d772f1cd3cb2637c8101e
DevDewangan16 <dewangandev977@gmail.com>
2025-11-04 16:47:46 +0530

    Enhanced the UI.

--- app/src/main/java/com/example/crimewatch/ui/SignInScreen.kt
+++ app/src/main/java/com/example/crimewatch/ui/SignInScreen.kt
@@ -2,6 +2,7 @@ package com.example.crimewatch.ui
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +13,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,49 +33,60 @@ import com.example.crimewatch.R
 @Composable
 fun SignInScreen(){
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(10.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(10.dp)
+        modifier = Modifier.background(color = Color(0xffE53131))
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.logo),
-            contentDescription ="Logo",
-            modifier = Modifier.size(282.dp),
-            contentScale = ContentScale.Fit,
-        )
-        Text(
-            text = "Welcome to CrimeWatch",
-            modifier = Modifier.fillMaxWidth(),
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            text = "Join the community and make your surroundings safer.",
-            modifier = Modifier.fillMaxWidth(),
-            fontSize = 20.sp,
-        )
-        OutlinedButton(onClick = { /*TODO*/ } ,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp),
-            shape = RoundedCornerShape(5.dp),
-            border = BorderStroke(2.dp,Color(0xffE53131))
-        ) {
-            Text(
-                text = "Continue with Google",
-                fontSize = 20.sp,
-                color = Color.Black
+        Card (
+            modifier = Modifier.padding(25.dp,120.dp,25.dp,0.dp),
+            shape = RoundedCornerShape(15.dp,15.dp,0.dp,0.dp),
+            colors = CardDefaults.cardColors(
+                Color.White
             )
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription ="Logo",
+                    modifier = Modifier.size(282.dp),
+                    contentScale = ContentScale.Fit,
+                )
+                Text(
+                    text = "Welcome to CrimeWatch",
+                    modifier = Modifier.fillMaxWidth(),
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Join the community and make your surroundings safer.",
+                    modifier = Modifier.fillMaxWidth(),
+                    fontSize = 20.sp,
+                )
+                OutlinedButton(onClick = { /*TODO*/ } ,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    shape = RoundedCornerShape(5.dp),
+                    border = BorderStroke(2.dp,Color(0xffE53131))
+                ) {
+                    Text(
+                        text = "Continue with Google",
+                        fontSize = 20.sp,
+                        color = Color.Black
+                    )
+                }
+                Text(
+                    text = "By continuing, you agree to CrimeWatch’s Terms & Privacy Policy.",
+                    modifier = Modifier.fillMaxWidth(),
+                    fontSize = 18.sp,
+                    color = Color(0xff7C7272),
+                    textAlign = TextAlign.Center
+                )
+            }
         }
-        Text(
-            text = "By continuing, you agree to CrimeWatch’s Terms & Privacy Policy.",
-            modifier = Modifier.fillMaxWidth(),
-            fontSize = 18.sp,
-            color = Color(0xff7C7272),
-            textAlign = TextAlign.Center
-        )
     }
-
 }
